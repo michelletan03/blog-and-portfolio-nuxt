92aca2ef3569fa99fe72df0ba3808ce5e38df2a2
Michelle Tan <53888443+michelletan03@users.noreply.github.com>
2024-07-02 19:25:31 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import { darkMode } from "./tailwind.config";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@nuxt/content'
+  ],
+  content: {
+    highlight: {
+      theme: {
+        default: 'min-light',
+        dark: 'min-dark'
+      }
+    }
+  },
+  colorMode: {
+    classSuffix: ''
+  }
+})
