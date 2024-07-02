c4a8fe6a92108ecefbd7a2035af4e3cb1ca6fdbf
Michelle Tan <53888443+michelletan03@users.noreply.github.com>
2024-07-02 19:27:47 +0900

    pages

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <article class="prose dark:prose-invert">
+      <ContentDoc path="/about"/>
+    </article>
+    <!-- <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div> -->
+</template>
+
+<!-- <script setup>
+definePageMeta({
+    layout: "another",
+
+});
+function enableCustomLayout () {
+  setPageLayout('another')
+}
+
+useHead({
+  title: 'About'
+
+
+})
+</script> -->
---
+++ pages/projects.vue
@@ -0,0 +1,6 @@
+<template>
+    <article class="prose dark:prose-invert">
+      <ContentDoc path="/projects"/>
+    </article>
+
+</template>
